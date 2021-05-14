dea6f519392eacb23a3e7c4f358b5c8713d7a3c0
Raghavan <oneraghavan@gmail.com>
2021-05-14 14:58:02 +0530

    Push latest code

--- assignments/3/keras2_with_attn_vis.py
+++ assignments/3/keras2_with_attn_vis.py
@@ -3,15 +3,15 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import argparse
-import pandas as pd
+import seaborn as sns
 from tensorflow.keras.layers import LSTM, Input, Dense, Embedding, GRU, SimpleRNN, Dropout, Concatenate
 from tensorflow.keras import Model
+from tensorflow.python.keras.layers import TimeDistributed
+import matplotlib.pyplot as plt
+
 import wandb
 from keras.optimizers import Adam
 from attention import AttentionLayer
-
-all_true = []
-all_pred = []
 
 
 # In[7]:
@@ -72,21 +72,25 @@ def get_attention_model(encoder_layers, decoder_layers, encoder_inputs, decoder_
     encoder_latent_dims = np.full(encoder_layers, hidden_size)
     decoder_latent_dims = np.full(decoder_layers, hidden_size)
 
-    outputs = Embedding(num_encoder_tokens, embedding_size)(encoder_inputs)
+    embedding_layer = Embedding(num_encoder_tokens, embedding_size)
+    outputs = embedding_layer(encoder_inputs)
     encoder_states = []
     cell = SimpleRNN
     if cell_type == 'lstm':
         cell = LSTM
     elif cell_type == 'gru':
         cell = GRU
+    input_layers = []
     for j in range(len(encoder_latent_dims))[::-1]:
         if cell_type == 'lstm':
-            outputs, h, c = cell(hidden_size, return_state=True, return_sequences=True, recurrent_dropout=dropout)(
-                outputs)
+            cell_ = cell(hidden_size, return_state=True, return_sequences=True, recurrent_dropout=dropout)
+            input_layers.append(cell_)
+            outputs, h, c = cell_(outputs)
             encoder_states += [(h, c)]
         else:
-            outputs, hidden = cell(hidden_size, return_state=True, return_sequences=True, recurrent_dropout=dropout)(
-                outputs)
+            cell_ = cell(hidden_size, return_state=True, return_sequences=True, recurrent_dropout=dropout)
+            input_layers.append(cell_)
+            outputs, hidden = cell_(outputs)
             encoder_states += [hidden]
             # Set up the decoder, setting the initial state of each layer to the state of the layer in the encoder
     # which is it's mirror (so for encoder: a->b->c, you'd have decoder initial states: c->b->a).
@@ -105,6 +109,8 @@ def get_attention_model(encoder_layers, decoder_layers, encoder_inputs, decoder_
         # outputs, dh, dc = output_layers[-1](outputs, initial_state=encoder_states[j])
 
     attn_layer = AttentionLayer(name='attention_layer')
+    print(encoder_out.shape)
+    print(outputs.shape)
     attn_out, attn_states = attn_layer([encoder_out, outputs])
 
     # Concat attention input and decoder GRU output
@@ -117,8 +123,47 @@ def get_attention_model(encoder_layers, decoder_layers, encoder_inputs, decoder_
     # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
 
     model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    model.summary()
+    """ Encoder (Inference) model """
+    encoder_inf_inputs = Input(batch_shape=(None, None), name='encoder_inf_inputs')
+    output = embedding_layer(encoder_inf_inputs)
+    encoder_states_inf = []
 
-    return decoder_dense, decoder_outputs, output_layers, encoder_states, model
+    for j in range(len(encoder_latent_dims))[::-1]:
+        temp = input_layers[len(encoder_latent_dims) - j - 1](output)
+
+        output, cur_states = temp[0], temp[1:]
+
+        encoder_states_inf += cur_states
+
+    encoder_model = Model(inputs=encoder_inf_inputs, outputs=[output, encoder_states_inf])
+
+    """ Decoder (Inference) model """
+    decoder_inf_inputs = Input(batch_shape=(None, 1, num_decoder_tokens), name='decoder_word_inputs')
+    encoder_inf_out = Input(batch_shape=(None, None, hidden_size), name='encoder_inf_states')
+    # decoder_init_state = Input(batch_shape=(batch_size, hidden_size), name='decoder_init')
+
+    decoder_states_inputs = []
+    decoder_states = []
+    d_outputs = decoder_inf_inputs
+    state_count = 2 if cell_type == 'lstm' else 1
+    encoder_inf_states = []
+    for j in range(len(decoder_latent_dims))[::-1]:
+        initial_state = [Input(shape=(decoder_latent_dims[j],)) for _ in range(state_count)]
+        temp = output_layers[len(decoder_latent_dims) - j - 1](d_outputs, initial_state=initial_state)
+
+        d_outputs, cur_states = temp[0], temp[1:]
+        encoder_inf_states += initial_state
+        decoder_states += cur_states
+        # decoder_states_inputs += current_state_inputs
+
+    attn_inf_out, attn_inf_states = attn_layer([encoder_inf_out, d_outputs])
+    decoder_inf_concat = Concatenate(axis=-1, name='concat')([d_outputs, attn_inf_out])
+    decoder_inf_pred = TimeDistributed(decoder_dense)(decoder_inf_concat)
+    decoder_model = Model(inputs=[encoder_inf_out, encoder_inf_states, decoder_inf_inputs],
+                          outputs=[decoder_inf_pred, attn_inf_states, decoder_states])
+
+    return decoder_dense, decoder_outputs, output_layers, encoder_states, model, attn_layer, encoder_out, encoder_model, decoder_model
 
 
 def manipulate_layered(decoder_layers, encoder_layers, encoder_states):
@@ -240,17 +285,15 @@ if __name__ == '__main__':
     callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
     from wandb.keras import WandbCallback
 
-    batch_size = 16  # Batch size for training.
-    epochs = 100  # Number of epochs to train for.
+    batch_size = 1024  # Batch size for training.
+    epochs = 1  # Number of epochs to train for.
 
     data_path = "/data/dakshina/dakshina_dataset_v1.0/ta/lexicons/ta.translit.sampled.train.tsv"
     val_data_path = "/data/dakshina/dakshina_dataset_v1.0/ta/lexicons/ta.translit.sampled.dev.tsv"
-    test_data_path = "/data/dakshina/dakshina_dataset_v1.0/ta/lexicons/ta.translit.sampled.test.tsv"
     input_characters = set()
     target_characters = set()
     input_texts, target_texts = get_data(data_path, input_characters, target_characters)
     val_input_texts, val_target_texts = get_data(val_data_path, input_characters, target_characters)
-    test_input_texts, test_target_texts = get_data(test_data_path, input_characters, target_characters)
 
     input_characters = sorted(list(input_characters))
     target_characters = sorted(list(target_characters))
@@ -268,17 +311,15 @@ if __name__ == '__main__':
                                                                                            num_encoder_tokens,
                                                                                            num_decoder_tokens)
 
-    test_encoder_input_data, test_decoder_input_data, test_decoder_target_data = prepare_data(test_input_texts,
-                                                                                              test_target_texts,
-                                                                                              num_encoder_tokens,
-                                                                                              num_decoder_tokens)
-
     encoder_inputs = Input(shape=(None,))
     decoder_inputs = Input(shape=(None, num_decoder_tokens))
 
-    decoder_dense, decoder_outputs, output_layers, encoder_states, model = get_model(encoder_layers, decoder_layers,
-                                                                                     encoder_inputs,
-                                                                                     decoder_inputs, cell_type, dropout)
+    decoder_dense, decoder_outputs, output_layers, encoder_states, model, attn_layer, encoder_out, encoder_model, decoder_model = get_attention_model(
+        encoder_layers,
+        decoder_layers,
+        encoder_inputs,
+        decoder_inputs,
+        cell_type, dropout)
 
     model.compile(
         optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
@@ -294,56 +335,96 @@ if __name__ == '__main__':
     model.save("s2s")
 
     # Define sampling models (modified for n-layer deep network)
-    encoder_model = Model(encoder_inputs, encoder_states)
-
-    d_outputs = decoder_inputs
-    decoder_states_inputs = []
-    decoder_states = []
-    decoder_latent_dims = np.full(decoder_layers, hidden_size)
-    for j in range(len(decoder_latent_dims))[::-1]:
-        state_count = 2 if cell_type == 'lstm' else 1
-        current_state_inputs = [Input(shape=(decoder_latent_dims[j],)) for _ in range(state_count)]
-
-        temp = output_layers[len(decoder_latent_dims) - j - 1](d_outputs, initial_state=current_state_inputs)
-
-        d_outputs, cur_states = temp[0], temp[1:]
-
-        decoder_states += cur_states
-        decoder_states_inputs += current_state_inputs
-
-    decoder_outputs = decoder_dense(d_outputs)
-    decoder_model = Model(
-        [decoder_inputs] + decoder_states_inputs,
-        [decoder_outputs] + decoder_states)
+    # encoder_model = Model(encoder_inputs, [encoder_states, encoder_out])
+    #
+    # d_outputs = decoder_inputs
+    # decoder_states_inputs = []
+    # decoder_states = []
+    # decoder_latent_dims = np.full(decoder_layers, hidden_size)
+    # for j in range(len(decoder_latent_dims))[::-1]:
+    #     state_count = 2 if cell_type == 'lstm' else 1
+    #     current_state_inputs = [Input(shape=(decoder_latent_dims[j],)) for _ in range(state_count)]
+    #
+    #     temp = output_layers[len(decoder_latent_dims) - j - 1](d_outputs, initial_state=current_state_inputs)
+    #
+    #     d_outputs, cur_states = temp[0], temp[1:]
+    #
+    #     decoder_states += cur_states
+    #     decoder_states_inputs += current_state_inputs
+    #
+    # print(encoder_out.shape)
+    # print(d_outputs.shape)
+    # attn_out, attn_states = attn_layer([encoder_out, d_outputs])
+    #
+    # # Concat attention input and decoder GRU output
+    # decoder_concat_input = Concatenate(axis=-1, name='concat_layer')([d_outputs, attn_out])
+    #
+    # decoder_outputs = decoder_dense(decoder_concat_input)
+    # decoder_model = Model(
+    #     [decoder_inputs, decoder_states_inputs],
+    #     [decoder_outputs] + decoder_states)
 
     reverse_input_char_index = dict((i, char) for char, i in input_token_index.items())
     reverse_target_char_index = dict((i, char) for char, i in target_token_index.items())
     max_decoder_seq_length = max([len(txt) for txt in target_texts])
 
 
+    def plot_attention_weights(encoder_inputs, attention_weights, en_id2word, fr_id2word):
+        mats = []
+        dec_inputs = []
+        for dec_ind, attn in attention_weights:
+            mats.append(attn.reshape(-1))
+            dec_inputs.append(dec_ind)
+        attention_mat = np.transpose(np.array(mats))
+
+        fig, ax = plt.subplots(figsize=(32, 32))
+        # ax.imshow(attention_mat)
+        #
+        # ax.set_xticks(np.arange(attention_mat.shape[1]))
+        # ax.set_yticks(np.arange(attention_mat.shape[0]))
+        #
+        output_chars = [fr_id2word[inp] if inp != 0 else "<Res>" for inp in dec_inputs]
+        # ax.set_xticklabels(output_chars)
+        input_chars = [en_id2word[inp] if inp != 0 else "<Res>" for inp in encoder_inputs]
+        # ax.set_yticklabels(input_chars)
+        #
+        # ax.tick_params(labelsize=32)
+        # ax.tick_params(axis='x', labelrotation=90)
+        #
+        # plt.savefig('attention.png')
+        
+        heatmap = sns.heatmap(attention_mat,xticklabels=output_chars,yticklabels=input_chars)
+
+        # plt.show()
+        heatmap.get_figure().savefig('attention.png')
     # In[12]:
 
     def decode_sequence(input_seq, encoder_model, decoder_model):
         # Encode the input as state vectors.
-        states_value = encoder_model.predict(input_seq)
+        states_value_and_out = encoder_model.predict(input_seq)
 
+        encoder_outs = states_value_and_out[0]
+        states_value = states_value_and_out[1]
         # Generate empty target sequence of length 1.
         target_seq = np.zeros((1, 1, num_decoder_tokens))
         # Populate the first character of target sequence with the start character.
         target_seq[0, 0, target_token_index['\t']] = 1.
-
+        attn_weights = []
         # Sampling loop for a batch of sequences
         # (to simplify, here we assume a batch of size 1).
         stop_condition = False
         decoded_sentence = []  # Creating a list then using "".join() is usually much faster for string creation
         states_value = manipulate_layered(decoder_layers, encoder_layers, states_value)
         while not stop_condition:
-            to_split = decoder_model.predict([target_seq] + states_value)
-
-            output_tokens, states_value = to_split[0], to_split[1:]
-
+            # print(target_seq.shape)
+            # print(states_value.shape)
+            # print(encoder_outs.shape)
+            to_split = decoder_model.predict([encoder_outs, states_value, target_seq])
+            output_tokens, attn_out, states_value = to_split
+            target_seq = output_tokens
             # Sample a token
             sampled_token_index = np.argmax(output_tokens[0, 0])
+            attn_weights.append((sampled_token_index, attn_out))
             sampled_char = reverse_target_char_index[sampled_token_index]
             decoded_sentence.append(sampled_char)
 
@@ -356,23 +437,30 @@ if __name__ == '__main__':
             target_seq = np.zeros((1, 1, num_decoder_tokens))
             target_seq[0, 0, sampled_token_index] = 1.
 
-        return "".join(decoded_sentence)
+        return "".join(decoded_sentence), attn_weights
 
 
     import random
 
-    predictions = []
-    for seq_index in range(len(test_input_texts)):
-        # seq_index = random.randint(0, len(val_input_texts))
+    attn_tuple = []
+    for seq_index in range(2):
+        seq_index = random.randint(0, len(val_input_texts))
         # Take one sequence (part of the training set)
         # for trying out decoding.
-        input_seq = test_encoder_input_data[seq_index: seq_index + 1]
-        decoded_sentence = decode_sequence(input_seq, encoder_model, decoder_model)
+        input_seq = encoder_input_data[seq_index: seq_index + 1]
+        decoded_sentence, attn_weights = decode_sequence(input_seq, encoder_model, decoder_model)
+        encoder_input = encoder_input_data[seq_index: seq_index + 1]
+        encoder_input_cleaned = []
+        for input in encoder_input[0]:
+            if input != 0:
+                encoder_input_cleaned.append(input)
+        plot_attention_weights(encoder_input_cleaned, attn_weights, reverse_input_char_index,
+                               reverse_target_char_index)
         print("-")
-        print("Input sentence:", test_input_texts[seq_index])
+        print("Input sentence:", input_texts[seq_index])
         print("Decoded sentence:", decoded_sentence)
-        print("target sentence:", test_target_texts[seq_index])
-        predictions.append((test_input_texts[seq_index], decoded_sentence,
-                            test_target_texts[seq_index].replace("\t", "").replace("\n", "")))
+        print("target sentence:", target_texts[seq_index])
 
-    pd.DataFrame(predictions, columns=["input", "predictions", "actual"]).to_csv("predictions.csv", index=False)
+import matplotlib.pyplot as plt
+
+
